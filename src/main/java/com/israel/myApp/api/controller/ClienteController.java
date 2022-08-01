8c78334028b24cd6c8edb04ffa2d56e8c9f5173a
raelnascimento <nascimentoisrael19@gmail.com>
2022-08-01 15:41:55 -0300

    Criando API myApp - v1.1

--- src/main/java/com/israel/myApp/api/controller/ClienteController.java
+++ src/main/java/com/israel/myApp/api/controller/ClienteController.java
@@ -8,9 +8,17 @@ package com.israel.myApp.api.controller;
 import com.israel.myApp.domain.model.Cliente;
 import com.israel.myApp.domain.repository.ClienteRepository;
 import java.util.List;
+import java.util.Optional;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -30,5 +38,23 @@ public class ClienteController {
     
         return clienteRepository.findAll();
     }
+    
+    @GetMapping("/{clienteid}")
+    public  ResponseEntity<Cliente> listaId(@PathVariable Long clienteid){
+        Optional <Cliente> cliente = clienteRepository.findById(clienteid);
+        
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente criar(@Valid @RequestBody Cliente cliente){
+        return clienteRepository.save(cliente);
+    }
+    
             
 }
