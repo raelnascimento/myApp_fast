6df99a6edb42eaa401ebd0a4a9bd4ee26cc6bcbe
raelnascimento <nascimentoisrael19@gmail.com>
2022-08-02 08:44:30 -0300

    Criando API myApp - v1.2

--- src/main/java/com/israel/myApp/api/controller/PedidoController.java
+++ src/main/java/com/israel/myApp/api/controller/PedidoController.java
@@ -10,8 +10,12 @@ import com.israel.myApp.domain.repository.PedidoRepository;
 import com.israel.myApp.domain.service.PedidoService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  *
@@ -32,5 +36,9 @@ public class PedidoController {
         return pedidoRepository.findAll();
     }
     
-    
+    @PostMapping("/teste")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Pedido criar (@RequestBody Pedido pedido){
+        return pedidoService.criar(pedido);
+    }
 }
--- src/main/java/com/israel/myApp/domain/service/PedidoService.java
+++ src/main/java/com/israel/myApp/domain/service/PedidoService.java
@@ -5,7 +5,10 @@
  */
 package com.israel.myApp.domain.service;
 
+import com.israel.myApp.domain.model.Pedido;
+import com.israel.myApp.domain.model.StatusPedido;
 import com.israel.myApp.domain.repository.PedidoRepository;
+import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,4 +21,11 @@ public class PedidoService {
     
     @Autowired
     private PedidoRepository pedidoRepository;
+    
+    public Pedido criar(Pedido pedido){
+     pedido.setDataHoraPedido(LocalDateTime.now());
+     pedido.setStatus(StatusPedido.CONFIRMADO);
+     
+     return pedidoRepository.save(pedido);
+    }
 }
